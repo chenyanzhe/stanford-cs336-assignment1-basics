43a009ba16d0cb25bd806f520c41084e67afaa37
Yanzhe Chen <yanzhe.cn@gmail.com>
2026-08-17 20:40:27 +0800

    Two more optimizations:
    -  parallelize the pre-tokenization
    -  regex pre-compile

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,10 +1,12 @@
 import os
-# import time
+import time
 import regex as re
 from collections import defaultdict
+from concurrent.futures import ProcessPoolExecutor
 from cs336_basics.pretokenization_example import find_chunk_boundaries
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+COMPILED_PAT = re.compile(PAT)
 
 
 def merge_pair(tokens: tuple[bytes, ...], pair: tuple[bytes, bytes]) -> tuple[bytes, ...]:
@@ -23,10 +25,44 @@ def merge_pair(tokens: tuple[bytes, ...], pair: tuple[bytes, bytes]) -> tuple[by
     return tuple(result)
 
 
-# Performance Records
-# 1. pre-tokenization: 0.046, merges: 2.212
-# 2. pre-tokenization: 0.104, merges: 1.216
-# 3. pre-tokenization: 0.106, merges: 0.222
+def pre_tokenization(args: tuple):
+    input_path, special_tokens, start, end = args
+    word_counts = defaultdict(int)
+    with open(input_path, "rb") as f:
+        f.seek(start)
+        chunk = f.read(end - start).decode("utf-8", errors="ignore")
+        # Remove special tokens before pre-tokenization.
+        if special_tokens:
+            corpora = re.split("|".join(map(re.escape, special_tokens)), chunk)
+        else:
+            corpora = [chunk]
+        # Pre-tokenization and count frequency.
+        for corpus in corpora:
+            for match in re.finditer(COMPILED_PAT, corpus):
+                word_counts[tuple(bytes([b]) for b in match.group().encode("utf-8"))] += 1
+    return word_counts
+
+
+# Performance records for test_train_bpe_speed
+# v1. pre-tokenization: 0.046s merges: 2.212s
+# v2. pre-tokenization: 0.104s merges: 1.216s
+# v3. pre-tokenization: 0.106s merges: 0.222s
+#
+# Perforamnce records for all 3 tests (v3):
+# test_train_bpe_speed
+#   pre-tokenization: 0.103s merges: 0.235s
+# test_train_bpe
+#   pre-tokenization: 0.044s merges: 0.225s
+# test_train_bpe_special_tokens
+#   pre-tokenization: 1.898s merges: 0.639s
+#
+# Performance records for all 3 tests (v4):
+# test_train_bpe_speed
+#   pre-tokenization: 0.099s merges: 0.222s
+# test_train_bpe
+#   pre-tokenization: 0.092s merges: 0.229s
+# test_train_bpe_special_tokens
+#   pre-tokenization: 0.549s merges: 0.694s
 def train_bpe(
     input_path: str | os.PathLike,
     vocab_size: int,
@@ -45,25 +81,19 @@ def train_bpe(
     if next_v >= vocab_size:
         return vocab, merges
 
-    # start_time = time.time()
+    start_time = time.time()
     with open(input_path, "rb") as f:
         num_processes = 4
         boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+    # Parallelize pre-tokenization work by sending each start/end pair to a set of processes.
+    tasks = [(input_path, special_tokens, start, end) for start, end in zip(boundaries[:-1], boundaries[1:])]
+    word_counts = defaultdict(int)  # pre-tokenized words and counts
+    with ProcessPoolExecutor(max_workers=num_processes) as executor:
+        for chunk_counts in executor.map(pre_tokenization, tasks):
+            for word, count in chunk_counts.items():
+                word_counts[word] += count
 
-        # The following is a serial implementation, but you can parallelize this
-        # by sending each start/end pair to a set of processes.
-        word_counts = defaultdict(int)  # pre-tokenized words and counts
-        for start, end in zip(boundaries[:-1], boundaries[1:]):
-            f.seek(start)
-            chunk = f.read(end - start).decode("utf-8", errors="ignore")
-            # Remove special tokens before pre-tokenization.
-            corpora = re.split("|".join(map(re.escape, special_tokens)), chunk)
-            # Pre-tokenization and count frequency.
-            for corpus in corpora:
-                for match in re.finditer(PAT, corpus):
-                    word_counts[tuple(bytes([b]) for b in match.group().encode("utf-8"))] += 1
-    # end_time = time.time()
-    # print("pre-tokenization took ", end_time - start_time)
+    end_time = time.time()
 
     # Build pair_counts and pair_to_words stats.
     pair_counts = defaultdict(int)  # byte pairs and counts.
@@ -73,7 +103,7 @@ def train_bpe(
             pair_counts[(i, j)] += count
             pair_to_words[(i, j)].add(word)
 
-    while next_v < vocab_size:
+    while next_v < vocab_size and pair_counts:
         # Merge the pair (a, b) that occurs the most.
         a, b = max(pair_counts, key=lambda pair: (pair_counts[pair], pair))
         merges.append((a, b))
@@ -99,7 +129,7 @@ def train_bpe(
                 if len(pair_to_words[(i, j)]) == 0:
                     del pair_to_words[(i, j)]
 
-    # end_time2 = time.time()
-    # print("merges took ", end_time2 - end_time)
+    end_time2 = time.time()
+    print(f"pre-tokenization: {end_time - start_time:.3f}s merges: {end_time2 - end_time:.3f}s")
 
     return vocab, merges
