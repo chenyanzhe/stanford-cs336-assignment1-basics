36a9e44e2dfe0b5244d27fdc721406c24fab3acd
Yanzhe Chen <yanzhe.cn@gmail.com>
2026-08-22 17:06:17 +0800

    Fully implements Tokenizer.
    All tests pass now.

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,5 +1,4 @@
-from collections import defaultdict
-from heapq import merge
+import heapq
 import pickle
 import regex as re
 from collections.abc import Iterable, Iterator
@@ -30,8 +29,9 @@ class Tokenizer:
     ):
         self.vocab = vocab
         self.reverse_vocab = {v: k for k, v in vocab.items()}
-        self.merges = merges
+        self.merge_to_index = {pair: i for i, pair in enumerate(merges)}
         self.special_tokens = set(special_tokens) if special_tokens is not None else set()
+        self.word_encode_cache = {}
 
         # Add special tokens to vocab and reverse_vocab if they are not already present
         next_v = len(vocab)
@@ -62,55 +62,44 @@ class Tokenizer:
             if token
         ]
 
+    def encode_word(self, word: bytes) -> list[int]:
+        if word in self.word_encode_cache:
+            return self.word_encode_cache[word]
+
+        heap = []
+        tokens = [bytes([b]) for b in word]
+        for pair in zip(tokens[:-1], tokens[1:]):
+            if pair in self.merge_to_index:
+                heapq.heappush(heap, (self.merge_to_index[pair], pair))
+
+        while heap:
+            _, pair = heapq.heappop(heap)
+            tokens = merge_pair(tokens, pair)
+
+            # Update the heap with new pairs formed after the merge
+            new_heap = []
+            for new_pair in zip(tokens[:-1], tokens[1:]):
+                if new_pair in self.merge_to_index:
+                    heapq.heappush(new_heap, (self.merge_to_index[new_pair], new_pair))
+            heap = new_heap
+
+        result = [self.reverse_vocab[t] for t in tokens]
+        self.word_encode_cache[word] = result
+
+        return result
+
     def encode(self, text: str) -> list[int]:
         if not text:
             return []
 
         pre_tokens = self.split_by_special_tokens(text)
-        words = []
-        word_to_id = {}  # dict[bytes, int]
-        word_tokens = []  # list(list[bytes])
-        pair_to_words = defaultdict(set)  # dict[tuple[bytes, bytes], set[int]]
-        next_word_id = 0
+        result = []
         for token in pre_tokens:
             if token in self.special_tokens:
-                words.append(token.encode("utf-8"))
+                result.append(self.reverse_vocab[token.encode("utf-8")])
             else:
-                split_workds = [match.group().encode("utf-8") for match in re.finditer(COMPILED_PAT, token)]
-                words.extend(split_workds)
-                for word in split_workds:
-                    if word in word_to_id:
-                        continue
-                    word_to_id[word] = next_word_id
-                    word_tokens.append(list(bytes([b]) for b in word))
-                    for pair in zip(word_tokens[-1][:-1], word_tokens[-1][1:]):
-                        pair_to_words[pair].add(next_word_id)
-                    next_word_id += 1
-
-        for pair_to_merge in self.merges:
-            for idx in list(pair_to_words[pair_to_merge]):
-                tokens = word_tokens[idx]
-                merged_tokens = merge_pair(tokens, pair_to_merge)
-                word_tokens[idx] = merged_tokens
-
-                old_pairs = set(zip(tokens[:-1], tokens[1:]))
-                new_pairs = set(zip(merged_tokens[:-1], merged_tokens[1:]))
-
-                for p in old_pairs:
-                    if p not in new_pairs:
-                        pair_to_words[p].discard(idx)
-
-                for p in new_pairs:
-                    if p not in old_pairs:
-                        pair_to_words[p].add(idx)
-
-        result = []
-        for word in words:
-            if word not in word_to_id:
-                result.append(self.reverse_vocab[word])
-                continue
-            for token in word_tokens[word_to_id[word]]:
-                result.append(self.reverse_vocab[token])
+                for match in re.finditer(COMPILED_PAT, token):
+                    result.extend(self.encode_word(match.group().encode("utf-8")))
 
         return result
 
