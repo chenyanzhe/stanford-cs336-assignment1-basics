fc1567c8e641dd8a7d7de46301d9463825c097d6
Yanzhe <4158637+chenyanzhe@users.noreply.github.com>
2026-08-30 06:33:14 +0000

    Implement causal multi-head self-attention.

---
+++ cs336_basics/multihead_self_attention.py
@@ -0,0 +1,50 @@
+import einops
+import torch
+from torch import nn
+
+from cs336_basics.linear import Linear
+from cs336_basics.rope import RotaryPositionalEmbedding
+from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
+
+
+class MultiHeadSelfAttention(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        num_heads: int,
+        max_seq_len: int,
+        theta: float | None = None,
+        token_positions: torch.Tensor | None = None,
+    ):
+        super().__init__()
+        self.num_heads = num_heads
+        self.L_q = Linear(d_model, d_model)
+        self.L_k = Linear(d_model, d_model)
+        self.L_v = Linear(d_model, d_model)
+        self.L_o = Linear(d_model, d_model)
+
+        self.use_rope = False
+        if theta is not None:
+            self.use_rope = True
+            self.rope = RotaryPositionalEmbedding(theta, d_model // num_heads, max_seq_len)
+            self.token_positions = token_positions
+
+        self.mask = torch.triu(torch.full((max_seq_len, max_seq_len), True))
+        self.mask = einops.rearrange(self.mask, "i j -> j i")
+        self.mask.unsqueeze(0).unsqueeze(0)
+
+    def forward(self, in_features: torch.Tensor) -> torch.Tensor:
+        Q = self.L_q(in_features)
+        K = self.L_k(in_features)
+        V = self.L_v(in_features)
+        Q_h = einops.rearrange(Q, "... seq_len (num_heads d_k) -> ... num_heads seq_len d_k", num_heads=self.num_heads)
+        K_h = einops.rearrange(K, "... seq_len (num_heads d_k) -> ... num_heads seq_len d_k", num_heads=self.num_heads)
+        V_h = einops.rearrange(V, "... seq_len (num_heads d_v) -> ... num_heads seq_len d_v", num_heads=self.num_heads)
+
+        if self.use_rope:
+            Q_h = self.rope(Q_h, self.token_positions)
+            K_h = self.rope(K_h, self.token_positions)
+
+        attention_h = scaled_dot_product_attention(Q_h, K_h, V_h, self.mask)
+        attention = einops.rearrange(attention_h, "... num_heads seq_len d_v -> ... seq_len (num_heads d_v)")
+        return self.L_o(attention)
--- tests/adapters.py
+++ tests/adapters.py
@@ -18,6 +18,7 @@ from cs336_basics.swiglu import SwiGLU
 from cs336_basics.rope import RotaryPositionalEmbedding
 from cs336_basics.softmax import softmax
 from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
+from cs336_basics.multihead_self_attention import MultiHeadSelfAttention
 
 
 def run_linear(
@@ -156,7 +157,12 @@ def run_multihead_self_attention(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    mhsa = MultiHeadSelfAttention(d_model, num_heads, in_features.shape[-2])
+    mhsa.L_q.load_state_dict({"W": q_proj_weight})
+    mhsa.L_k.load_state_dict({"W": k_proj_weight})
+    mhsa.L_v.load_state_dict({"W": v_proj_weight})
+    mhsa.L_o.load_state_dict({"W": o_proj_weight})
+    return mhsa(in_features)
 
 
 def run_multihead_self_attention_with_rope(
@@ -196,7 +202,12 @@ def run_multihead_self_attention_with_rope(
         Float[Tensor, " ... sequence_length d_model"]: Tensor with the output of running your optimized, batched multi-headed attention
         implementation with the given QKV projection weights and input features.
     """
-    raise NotImplementedError
+    mhsa = MultiHeadSelfAttention(d_model, num_heads, in_features.shape[-2], theta, token_positions)
+    mhsa.L_q.load_state_dict({"W": q_proj_weight})
+    mhsa.L_k.load_state_dict({"W": k_proj_weight})
+    mhsa.L_v.load_state_dict({"W": v_proj_weight})
+    mhsa.L_o.load_state_dict({"W": o_proj_weight})
+    return mhsa(in_features)
 
 
 def run_rope(
