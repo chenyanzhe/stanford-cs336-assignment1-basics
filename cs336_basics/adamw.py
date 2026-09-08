bab797a2fa0b7003575402d673bf0f683ada9f81
Yanzhe Chen <yanzhe.cn@gmail.com>
2026-09-08 20:18:45 +0800

    Improve AdamW implementation: update t, m, v in-place.

--- cs336_basics/adamw.py
+++ cs336_basics/adamw.py
@@ -15,7 +15,7 @@ class AdamW(torch.optim.Optimizer):
     ):
         if lr < 0:
             raise ValueError(f"Invalid learning rate: {lr}")
-        if betas[0] < 0.0 or betas[0] > 1.0 or betas[1] < 0.0 or betas[1] > 1.0:
+        if betas[0] < 0.0 or betas[0] >= 1.0 or betas[1] < 0.0 or betas[1] >= 1.0:
             raise ValueError(f"Invalid betas: {betas}")
         if weight_decay < 0.0:
             raise ValueError(f"Invalid weight decay: {weight_decay}")
@@ -37,27 +37,21 @@ class AdamW(torch.optim.Optimizer):
                     continue
 
                 state = self.state[p]  # Get state associated with p
-                grad = p.grad.data  # Get the gradient of loss with respect to p.
                 if "t" not in state:
                     state["t"] = 1
                 if "m" not in state:
-                    state["m"] = torch.zeros_like(grad)
+                    state["m"] = torch.zeros_like(p.grad)
                 if "v" not in state:
-                    state["v"] = torch.zeros_like(grad)
+                    state["v"] = torch.zeros_like(p.grad)
 
-                t = state.get("t")  # Get iteration number from the state.
-                m = state.get("m")  # Get the first moment estimate.
-                v = state.get("v")  # Get the second moment estimate.
                 lr = group["lr"]  # Get the learning rate.
                 lr_t = (
-                    lr * math.sqrt(1.0 - b2**t) / (1.0 - b1**t)
+                    lr * math.sqrt(1.0 - b2 ** state["t"]) / (1.0 - b1 ** state["t"])
                 )  # Compute the adjusted learning rate for iteration t.
-                p.data -= lr * weight_decay * p.data  # Apply weight decay.
-                m = b1 * m + (1 - b1) * grad  # Update the first moment estimate.
-                v = b2 * v + (1 - b2) * (grad**2)  # Update the second moment estimate.
-                p.data -= lr_t * m / (torch.sqrt(v) + eps)  # Apply the moment adjusted weight updates.
-                state["t"] = t + 1  # Increment iteration number.
-                state["m"] = m  # Save the first moment estimation for iteration t.
-                state["v"] = v  # Save the second moment estimation for iteration t.
+                p -= lr * weight_decay * p  # Apply weight decay.
+                state["m"].mul_(b1).add_(p.grad, alpha=1 - b1)  # Update the first moment estimate.
+                state["v"].mul_(b2).add_(p.grad**2, alpha=1 - b2)  # Update the second moment estimate.
+                p -= lr_t * state["m"] / (torch.sqrt(state["v"]) + eps)  # Apply the moment adjusted weight updates.
+                state["t"] += 1  # Increment iteration number.
 
         return loss
