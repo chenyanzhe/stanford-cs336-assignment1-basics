02a5e03ec61ac7614eeef297b63b008cc04aa5d9
chenyanzhe <yanzhe.cn@gmail.com>
2026-09-23 15:12:55 +0000

    Implement and connect silu in swiglu and test adapters

--- cs336_basics/swiglu.py
+++ cs336_basics/swiglu.py
@@ -4,6 +4,10 @@ from torch import nn
 from cs336_basics.linear import Linear
 
 
+def silu(in_features: torch.Tensor):
+    return in_features * torch.sigmoid(in_features)
+
+
 class SwiGLU(nn.Module):
     def __init__(self, d_model: int, d_ff: int, device: torch.device | None = None, dtype: torch.dtype | None = None):
         super().__init__()
@@ -12,5 +16,4 @@ class SwiGLU(nn.Module):
         self.w3 = Linear(d_model, d_ff, device=device, dtype=dtype)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        w1_x = self.w1(x)
-        return self.w2(torch.mul(torch.mul(w1_x, torch.sigmoid(w1_x)), self.w3(x)))
+        return self.w2(silu(self.w1(x)) * self.w3(x))
--- tests/adapters.py
+++ tests/adapters.py
@@ -22,7 +22,7 @@ from cs336_basics.rmsnorm import RMSNorm
 from cs336_basics.rope import RotaryPositionalEmbedding
 from cs336_basics.scaled_dot_product_attention import scaled_dot_product_attention
 from cs336_basics.softmax import softmax
-from cs336_basics.swiglu import SwiGLU
+from cs336_basics.swiglu import SwiGLU, silu
 from cs336_basics.tokenizer import Tokenizer
 from cs336_basics.train_bpe import train_bpe
 from cs336_basics.transformer_block import TransformerBlock
@@ -456,7 +456,7 @@ def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
         Float[Tensor,"..."]: of with the same shape as `in_features` with the output of applying
         SiLU to each element.
     """
-    raise NotImplementedError
+    return silu(in_features)
 
 
 def run_get_batch(
